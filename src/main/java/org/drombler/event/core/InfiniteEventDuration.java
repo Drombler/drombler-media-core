95b90675f23ec748b2ef90ad038c02d19f85978c
Florian Brunner <fbrunnerreg@gmx.ch>
2020-05-18 22:38:25 +0200

    - unit test - fixes

--- src/main/java/org/drombler/event/core/InfiniteEventDuration.java
+++ src/main/java/org/drombler/event/core/InfiniteEventDuration.java
@@ -1,24 +1,30 @@
 package org.drombler.event.core;
 
+import org.softsmithy.lib.text.FormatException;
+
+import java.io.IOException;
+
 /**
- *
  * @author Florian
  */
 
 // not used yet
-public class InfiniteEventDuration implements EventDuration{
+public class InfiniteEventDuration implements EventDuration {
     private static final InfiniteEventDuration INSTANCE = new InfiniteEventDuration();
-    
-    private InfiniteEventDuration(){
+
+    private InfiniteEventDuration() {
     }
 
-    private static final InfiniteEventDuration getInstance(){
+    private static final InfiniteEventDuration getInstance() {
         return INSTANCE;
     }
-    
+
     @Override
-    public String getDirName() {
-        return "infinite";
+    public Appendable formatDirName(Appendable appendable) throws FormatException {
+        try {
+            return appendable.append("infinite");
+        } catch (IOException e) {
+            throw new FormatException(e.getMessage(), e);
+        }
     }
-    
 }
