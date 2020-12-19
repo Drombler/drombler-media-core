a464da458e40ff6f0aac9c5ad0170c7c36dc6939
Florian Brunner <fbrunnerreg@gmx.ch>
2020-12-19 10:27:29 +0100

    - improvements

--- src/main/java/org/drombler/media/core/MediaStorageContentType.java
+++ src/main/java/org/drombler/media/core/MediaStorageContentType.java
@@ -8,29 +8,30 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Getter
-public enum MediaStorageType {
+public enum MediaStorageContentType {
     SHARED_EVENTS(""),
     PRIVATE_EVENTS("P-"),
     OTHER_EVENTS("A-"), // Andere
-    BUSINESS_EVENTS("G-"),  // Geschäft
+    BUSINESS("G-"),  // Geschäft
+    MEDICAL("M-"),
     THINGS("X-"); // Photos von Dingen
 
-    private static final Map<String, MediaStorageType> MEDIA_STORAGE_TYPE_MAP = Arrays.stream(values())
-            .collect(Collectors.toMap(MediaStorageType::getPrefix, Function.identity()));
+    private static final Map<String, MediaStorageContentType> MEDIA_STORAGE_CONTENT_TYPE_MAP = Arrays.stream(values())
+            .collect(Collectors.toMap(MediaStorageContentType::getPrefix, Function.identity()));
 
     private String prefix;
 
-    MediaStorageType(String prefix) {
+    MediaStorageContentType(String prefix) {
         this.prefix = prefix;
     }
 
     public static boolean isKnownPrefix(String prefix) {
-        return MEDIA_STORAGE_TYPE_MAP.containsKey(prefix);
+        return MEDIA_STORAGE_CONTENT_TYPE_MAP.containsKey(prefix);
     }
 
-    public static MediaStorageType getByPrefix(String prefix) {
+    public static MediaStorageContentType getByPrefix(String prefix) {
         if (isKnownPrefix(prefix)) {
-            return MEDIA_STORAGE_TYPE_MAP.get(prefix);
+            return MEDIA_STORAGE_CONTENT_TYPE_MAP.get(prefix);
         } else {
             throw new IllegalArgumentException("Unknown prefix: " + prefix);
         }
--- src/main/java/org/drombler/media/photo/core/PhotoMediaCategoryProvider.java
+++ src/main/java/org/drombler/media/photo/core/PhotoMediaCategoryProvider.java
@@ -1,11 +1,14 @@
 package org.drombler.media.photo.core;
 
-import java.util.Arrays;
-import static java.util.Arrays.asList;
-import java.util.HashSet;
 import org.drombler.media.core.MediaCategory;
 import org.drombler.media.core.MediaCategoryProvider;
 import org.drombler.media.core.MediaCategoryVariant;
+import org.drombler.media.core.protocol.json.MediaCategoryType;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static java.util.Arrays.asList;
 
 /**
  *
@@ -13,7 +16,7 @@ import org.drombler.media.core.MediaCategoryVariant;
  */
 public class PhotoMediaCategoryProvider implements MediaCategoryProvider {
 
-    private final MediaCategory photoMediaCategory = new MediaCategory("photo", Arrays.asList(
+    private final MediaCategory photoMediaCategory = new MediaCategory(MediaCategoryType.PHOTO, Arrays.asList(
             new MediaCategoryVariant(new HashSet<>(asList("image/jpeg")), new HashSet<>(Arrays.asList(".jpeg", ".jpg"))),
             new MediaCategoryVariant(new HashSet<>(asList("image/png")), new HashSet<>(Arrays.asList(".png")))
     ));
